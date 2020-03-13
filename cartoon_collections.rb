d0178639094a4d7b2582eebd709907219e7f5a21
Matt Gordon <bigmattsize21@gmail.com>
2020-03-13 19:11:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,6 +23,7 @@ end
 end
 
 def find_the_cheese(cheddar_cheese)
+  
   block_return_values = []
   while cheese_types < cheddar_cheese.length
     block_return_values << yield(cheddar_cheese[cheese_types])
