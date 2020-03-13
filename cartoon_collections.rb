3576fa1a606bc3feb020af31d025dea35dfa6409
Matt Gordon <bigmattsize21@gmail.com>
2020-03-13 18:32:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls (calls_long)
   block_return_values = []
   while i < calls_long.length
     block_return_values <<  
-yield calls_long[i] 
+yield (calls_long[i])
   end
  
   if block_return_values.include?(false)
