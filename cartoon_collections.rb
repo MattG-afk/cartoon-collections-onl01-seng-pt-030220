ddaa2200dd685ba3ff65c6ab31ab90ab293563cb
Matt Gordon <bigmattsize21@gmail.com>
2020-03-13 19:50:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,7 @@ end
 def find_the_cheese(cheddar_cheese)
    cheese_types = ["cheddar", "gouda", "camembert"]
     cheese_types.find do |cheese|
-      if cheese == true
+      if cheese_types 
         true
       end
     nil
